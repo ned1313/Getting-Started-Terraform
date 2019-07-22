da61dc8f4cc1ca5770cbd1d2e7fa91bfdab81128
Ned Bellavance <ned.bellavance@gmail.com>
2019-07-22 08:38:25 -0400

    m1 0.12 updates

--- m1/moduleone.tf
+++ m1/moduleone.tf
@@ -5,22 +5,43 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
-
-#Create a key pair with this name in AWS or change to an existing key pair name
-variable "key_name" {
-  default = "PluralsightKeys" 
-}
+variable "key_name" {}
 
 ##################################################################################
 # PROVIDERS
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   #Note if you change the region, you will need to update the AMI
-  region     = "us-east-1" 
+  region = "us-east-1"
 }
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 
 ##################################################################################
 # RESOURCES
@@ -32,40 +53,43 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name = "nginx_demo"
+  name        = "nginx_demo"
   description = "Allow ports for nginx demo"
-  vpc_id = "${aws_default_vpc.default.id}"
+  vpc_id      = aws_default_vpc.default.id
 
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "nginx" {
   #Note if you change the region, you will need to update the AMI
-  ami           = "ami-c58c1dd3" 
-  instance_type = "t2.micro"
-  key_name        = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   connection {
+    type        = "ssh"
+    host        = self.public_ip
     user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
+    private_key = file(var.private_key_path)
+
   }
 
   provisioner "remote-exec" {
@@ -81,5 +105,5 @@ resource "aws_instance" "nginx" {
 ##################################################################################
 
 output "aws_instance_public_dns" {
-    value = "${aws_instance.nginx.public_dns}"
+  value = aws_instance.nginx.public_dns
 }
