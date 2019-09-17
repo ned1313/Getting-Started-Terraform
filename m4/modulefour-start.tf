11d8108be33c84955a042c0641359b13f333bf89
Ned Bellavance <ned.bellavance@gmail.com>
2019-09-17 16:51:20 -0400

    module 4 and 5 updates

--- m4/modulefour-start.tf
+++ m4/modulefour-start.tf
@@ -5,8 +5,9 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
-variable "key_name" {
-  default = "PluralsightKeys"
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
 }
 variable "network_address_space" {
   default = "10.1.0.0/16"
@@ -14,18 +15,15 @@ variable "network_address_space" {
 variable "subnet1_address_space" {
   default = "10.1.0.0/24"
 }
-variable "subnet2_address_space" {
-  default = "10.1.1.0/24"
-}
 
 ##################################################################################
 # PROVIDERS
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 
 ##################################################################################
@@ -34,63 +32,70 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 ##################################################################################
 # RESOURCES
 ##################################################################################
 
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.network_address_space}"
+  cidr_block = var.network_address_space
   enable_dns_hostnames = "true"
 
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block        = "${var.subnet1_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.subnet1_address_space
+  vpc_id            = aws_vpc.vpc.id
   map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-}
-
-resource "aws_subnet" "subnet2" {
-  cidr_block        = "${var.subnet2_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
 
 }
 
 # ROUTING #
 resource "aws_route_table" "rtb" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
 resource "aws_route_table_association" "rta-subnet1" {
-  subnet_id      = "${aws_subnet.subnet1.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
-}
-
-resource "aws_route_table_association" "rta-subnet2" {
-  subnet_id      = "${aws_subnet.subnet2.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
 }
 
 # SECURITY GROUPS #
 # Nginx security group 
 resource "aws_security_group" "nginx-sg" {
   name        = "nginx_sg"
-  vpc_id      = "${aws_vpc.vpc.id}"
+  vpc_id      = aws_vpc.vpc.id
 
   # SSH access from anywhere
   ingress {
@@ -119,22 +124,33 @@ resource "aws_security_group" "nginx-sg" {
 
 # INSTANCES #
 resource "aws_instance" "nginx1" {
-  ami           = "ami-c58c1dd3"
+  ami           = data.aws_ami.aws-linux.id
   instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.subnet1.id}"
-  vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
-  key_name        = "${var.key_name}"
+  subnet_id     = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  key_name        = var.key_name
 
   connection {
+    type        = "ssh"
+    host        = self.public_ip
     user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
+    private_key = file(var.private_key_path)
+
   }
 
-  provisioner "remote-exec" {
+    provisioner "remote-exec" {
     inline = [
       "sudo yum install nginx -y",
       "sudo service nginx start",
-      "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
+      "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
+      "sudo pip install s3cmd",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
+      "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
+      "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
+      "sudo logrotate -f /etc/logrotate.conf"
+      
     ]
   }
 }
@@ -144,5 +160,5 @@ resource "aws_instance" "nginx1" {
 ##################################################################################
 
 output "aws_instance_public_dns" {
-    value = "${aws_instance.nginx1.public_dns}"
+    value = aws_instance.nginx1.public_dns
 }
