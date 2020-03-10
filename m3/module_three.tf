6aeab3bf1331ba712ab672d65c308fc48f4de1d1
AGUSTIN MERINO <agustin.merino@globant.com>
2020-03-10 16:36:32 -0500

    Se agrega subnet2, balanceador de carga y una segunda instancia

--- m3/module_three.tf
+++ m3/module_three.tf
@@ -10,6 +10,20 @@ variable "region" {
   default = "us-west-2"
 }
 
+# CIDR for VPC
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+
+# CIDR for subnet 1
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
+# CIDR for subnet 2
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
 ##################################################################################
 # PROVIDERS
 ##################################################################################
@@ -23,6 +37,9 @@ provider "aws" {
 ##################################################################################
 # DATA
 ##################################################################################
+
+data "aws_availability_zones" "available" {}
+
 
 data "aws_ami" "aws-linux" {
   most_recent = true
@@ -50,21 +67,66 @@ data "aws_ami" "aws-linux" {
 ##################################################################################
 
 #This uses the default VPC.  It WILL NOT delete it on destroy.
-resource "aws_default_vpc" "default" {
+
+#NETWORKING
+
+# Creation of VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
 
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "nginx_demo"
-  description = "Allow ports for nginx demo"
-  vpc_id      = aws_default_vpc.default.id
+# Creation of Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Creation of Subnet 1
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+}
+
+# Creation of Subnet 2
+resource "aws_subnet" "subnet2" {
+  cidr_block              = var.subnet2_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+}
+
+# Creation of Route Table
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+# Creation relationship between Subnet 1 and RouteTable
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+# Creation relationship between Subnet 2 and RouteTable
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_security_group" "elb-sg" {
+  name        = "nginx-elb-sg"
+  description = "Allow ports for nginx demo elb"
+  vpc_id      = aws_vpc.vpc.id
+
+  
   ingress {
     from_port   = 80
     to_port     = 80
@@ -79,19 +141,71 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_instance" "nginx" {
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx-sg"
+  description = "Allow ports for nginx instance"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.network_address_space]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Creation Load Balancer
+
+resource "aws_elb" "elb_web"{
+  name = "nginx-elb"
+  subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups = [aws_security_group.elb-sg.id]
+  instances       = [aws_instance.nginx1.id, aws_instance.nginx2.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+resource "aws_instance" "nginx1" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  user_data = "${file("userdata.txt")}"
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  subnet_id              = aws_subnet.subnet1.id
+  user_data = "${file("userdatablue.txt")}"
 
   tags = {
-    Name = "Devops Terraform Test"
-  }
+    Name = "Devops Terraform Test Blue"
+  }  
+}
 
-  
+resource "aws_instance" "nginx2" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  subnet_id              = aws_subnet.subnet2.id
+  user_data = "${file("userdatagreen.txt")}"
+
+  tags = {
+    Name = "Devops Terraform Test Green"
+  }  
 }
 
 #################################################################################
@@ -99,5 +213,5 @@ resource "aws_instance" "nginx" {
 ##################################################################################
 
 output "aws_instance_public_dns" {
-  value = aws_instance.nginx.public_dns
+  value = aws_elb.elb_web.dns_name
 }
