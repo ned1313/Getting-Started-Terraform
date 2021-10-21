01c918e9435ca7f02e194ab9286588b0885c5b77
ned1313 <ned.bellavance@gmail.com>
2021-10-21 10:15:49 -0400

    change command order for m6

--- commands/m6_commands.sh
+++ commands/m6_commands.sh
@@ -13,9 +13,11 @@ $env:AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
 # Now we can remove the variables from our configuration
 
 # Once the updates are complete we'll run the standard workflow
+terraform fmt
+
 terraform init
 terraform validate
-terraform fmt
+
 terraform plan -out m6.tfplan
 terraform apply m6.tfplan
 
