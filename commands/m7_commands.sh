736662c01c59c4db7f43a69c26e75acc774c840b
ned1313 <ned.bellavance@gmail.com>
2021-10-25 15:35:41 -0400

    add environment variables

--- commands/m7_commands.sh
+++ commands/m7_commands.sh
@@ -11,12 +11,21 @@ cidrsubnet(var.vpc_cidr_block, 8, 0)
 cidrhost(cidrsubnet(var.vpc_cidr_block, 8, 0),5)
 lookup(local.common_tags, "company", "Unknown")
 lookup(local.common_tags, "missing", "Unknown")
-local.s3_bucket_name
+local.common_tags
 
 # Update the configuration by adding count and for_each loops and
 # adding functions where helpful.
 
-terraform validate
 terraform fmt
+terraform validate
+
+# For Linux and MacOS
+export AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=YOUR_SECRET_ACCESS_KEY
+
+# For PowerShell
+$env:AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY"
+$env:AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
+
 terraform plan -out m7.tfplan
 terraform apply m7.tfplan
