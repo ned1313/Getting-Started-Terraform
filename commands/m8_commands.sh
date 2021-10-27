8df2afbc8f6f9c876fa5a819f2aecc60578afb97
ned1313 <ned.bellavance@gmail.com>
2021-10-27 09:29:58 -0400

    m8 commands update

--- commands/m8_commands.sh
+++ commands/m8_commands.sh
@@ -3,7 +3,26 @@
 # of the official VPC module on the Terraform registry and a module we
 # write ourselves for s3 resources associated with web applications.
 
+terraform console
+
+# Test out the range function
+range(var.vpc_subnet_count)
+# Try it in a for expression
+[for subnet in range(var.vpc_subnet_count): cidrsubnet(var.vpc_cidr_block, 8, subnet)]
+
+# Prepare config
+terraform init
+terraform fmt -recursive
 terraform validate
-terraform fmt
+
+# For Linux and MacOS
+export AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=YOUR_SECRET_ACCESS_KEY
+
+# For PowerShell
+$env:AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY"
+$env:AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
+
+# Update the deployment
 terraform plan -out m8.tfplan
 terraform apply m8.tfplan
