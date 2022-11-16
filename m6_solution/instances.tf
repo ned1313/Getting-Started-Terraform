41f3563d5acfc4ff8458bc447b4947896581ea9f
daniel packard <daniel@spectre3d.io>
2022-11-16 12:34:21 -0600

    Add missing iam_instance_profile and depends_on to m6_solution nginx2 config

--- m6_solution/instances.tf
+++ m6_solution/instances.tf
@@ -39,6 +39,8 @@ resource "aws_instance" "nginx2" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet2.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+  depends_on             = [aws_iam_role_policy.allow_s3_all]
 
   user_data = <<EOF
 #! /bin/bash
